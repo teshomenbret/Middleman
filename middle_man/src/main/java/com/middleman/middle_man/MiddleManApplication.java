a1caa3809f10990b54191123467b190cee201010
surafel getahun <surafelgetahun01@gmail.com>
2022-05-03 14:52:48 +0300

    starting

---
+++ middle_man/src/main/java/com/middleman/middle_man/MiddleManApplication.java
@@ -0,0 +1,13 @@
+package com.middleman.middle_man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleManApplication.class, args);
+	}
+
+}
